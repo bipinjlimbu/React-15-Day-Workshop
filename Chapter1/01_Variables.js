8168d4ab5d206e1c486495eef7bdd14915a39ed6
bipinjlimbu <bipinlimbu712@gmail.com>
2025-09-01 09:36:21 +0545

    Variables Completed in JS

---
+++ Chapter1/01_Variables.js
@@ -0,0 +1,23 @@
+//Const
+const data = "GOAT";
+const value = 7;
+const status = true;
+// value = 12; // Error 
+{
+    const value = 12;
+    console.log(value);
+}
+
+console.log(data);
+console.log(value);
+console.log(status);
+
+//Let
+let name = "Bipin";
+name = "CR7" //No Error
+{
+    let age = 19
+    console.log(age); //No error
+}
+console.log(name)
+//console.log(age) //error
