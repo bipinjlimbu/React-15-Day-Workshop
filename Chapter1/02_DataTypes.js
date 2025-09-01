4424cffba03fe6bb1230ce593aea691ef4c7dae9
bipinjlimbu <bipinlimbu712@gmail.com>
2025-09-01 10:13:08 +0545

    Primitive Data Types Completed in JS

--- Chapter1/02_DataTypes.js
+++ Chapter1/02_DataTypes.js
@@ -1,3 +1,9 @@
+//Primitive
+console.log(typeof 1);
+console.log(typeof "Bipin");
+console.log(typeof true);
+console.log(typeof null); //Bug shows Object instead of Null
+console.log(typeof undefined);
 //Non Primitive
 //Object
 const obj = {
@@ -7,6 +13,10 @@ const obj = {
     faculty:"BCA",
     semester:"3rd"
 };
+console.log(obj);
+console.log("After Changing Semester:");
 obj.semester = "4th";
 console.log(obj);
-console.log(obj.name);
+
+//Array
+const arr = [1,2,3,4,5,6,7];
