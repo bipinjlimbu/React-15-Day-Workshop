0bde1895db407054633785fe429c0ab4b28cf858
bipinjlimbu <bipinlimbu712@gmail.com>
2025-09-02 09:52:32 +0545

    If Else Completed in JS

---
+++ Chapter1/05_Conditional.js
@@ -0,0 +1,23 @@
+//if - else
+let profit = 5000;
+let loss = 2000;
+
+if (profit<loss){
+    console.log("You are in Loss");
+}
+else{
+    console.log("You are in Profit");
+}
+
+//if - else if - else
+let age = 9;
+
+if (age<18){
+    console.log("You are too Young to Drive.");
+}
+else if (age>80){
+    console.log("You are too Old to Drive");
+}
+else{
+    console.log("You are Ready to Drive");
+}
