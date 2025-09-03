9230ed34c91aa56615c311b8c6154a35ead407e1
bipinjlimbu <bipinlimbu712@gmail.com>
2025-09-03 10:12:55 +0545

    Array Completed in JS

---
+++ Chapter1/07_Array.js
@@ -0,0 +1,18 @@
+const arr = ["IshanGeh","AnushGopte","RuksanKalluBoii"];
+console.log(arr);
+console.log(arr.length);
+console.log(arr);
+console.log(arr.shift())
+console.log(arr);
+console.log(arr.unshift("BipinGOAT"))
+console.log(arr);
+console.log(arr.pop())
+console.log(arr);
+console.log(arr.push("IshanBunu"))
+console.log(arr);
+console.log(arr.at(-1));
+console.log(arr);
+console.log(arr.slice(1,));
+console.log(arr);
+console.log(arr.splice(1,1,"AnushThanos"));
+console.log(arr);
