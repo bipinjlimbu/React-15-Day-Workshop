a5367bec11216de0534a773a84d5aef33ab9b572
bipinjlimbu <bipinlimbu712@gmail.com>
2025-09-04 08:55:07 +0545

    Map and Filer Completed in JS

---
+++ JavaScript/08_MapFilter.js
@@ -0,0 +1,9 @@
+//Map
+const arr = [1,2,3,4,5];
+const value = arr.map(number=>number>2)
+console.log(value)
+
+//Filer
+const ary = [1,2,3,4,5];
+const val = ary.filter(number=>number>2)
+console.log(val)
