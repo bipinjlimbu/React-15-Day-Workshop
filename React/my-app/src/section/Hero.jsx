91e17412f4269dc1a6c4d29c8e736f14c2142cfd
bipinjlimbu <bipinlimbu712@gmail.com>
2025-09-13 16:28:19 +0545

    Hero Section CSS Completed in React

--- React/my-app/src/section/Hero.jsx
+++ React/my-app/src/section/Hero.jsx
@@ -1,6 +1,7 @@
+import myHero from "../style/Hero.module.css";
 const Hero = () => {
   return (
-    <section className="hero">
+    <section className={myHero.hero}>
       <h2>Welcome to My App</h2>
       <p>This is a simple hero section.</p>
     </section>
