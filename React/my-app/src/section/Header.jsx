f1be48bd14b1ba0293f46bf12aec7971d6177fe8
bipinjlimbu <bipinlimbu712@gmail.com>
2025-09-13 19:52:32 +0545

    Global CSS Completed in React

--- React/my-app/src/section/Header.jsx
+++ React/my-app/src/section/Header.jsx
@@ -3,8 +3,8 @@ import myHead from '../style/Header.module.css';
 const Header = () => {
   return (
     <header className={myHead.header}>
-      <h1>My App</h1>
-        <nav>
+      <h1 className={myHead.logo}>My App</h1>
+        <nav lassName={myHead.nav}>
           <ul>
             <li><a href="#Home">Home</a></li>
             <li><a href="#About">About</a></li>
