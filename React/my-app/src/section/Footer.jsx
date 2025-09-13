8ef32eae4055a9da8d8e589a0c860b8b44252891
bipinjlimbu <bipinlimbu712@gmail.com>
2025-09-13 16:36:10 +0545

    Footer Section CSS Completed in React

--- React/my-app/src/section/Footer.jsx
+++ React/my-app/src/section/Footer.jsx
@@ -2,7 +2,7 @@ import myFoot from '../style/Footer.module.css';
 
 const Footer = () => {
   return (
-    <footer className="footer">
+    <footer className={myFoot.footer}>
       <p>&copy; 2023 My App. All rights reserved.</p>
     </footer>
   );
