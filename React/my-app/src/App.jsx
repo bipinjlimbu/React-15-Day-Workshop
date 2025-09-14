4f03d8d2bd33e3e3de9d3cd08c253b9344ffa996
bipinjlimbu <bipinlimbu712@gmail.com>
2025-09-14 18:07:24 +0545

    All Section Upgraded CSS Completed in React

--- React/my-app/src/App.jsx
+++ React/my-app/src/App.jsx
@@ -1,14 +1,15 @@
 import Footer from "./section/Footer"
 import Header from "./section/Header"
 import Hero from "./section/Hero"
+import './global.css'
 
 export const App=()=>{
 
   return (
-    <>
+     <div className="app">
       <Header/>
       <Hero/>
       <Footer/>
-    </>
+    </div>
 )
 }
