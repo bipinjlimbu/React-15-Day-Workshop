50fde3f9eb8d5a7acff8438efc97b590ad8583d5
bipinjlimbu <bipinlimbu712@gmail.com>
2025-09-14 18:09:11 +0545

    Unwanted Files Removal Completed in React

--- React/my-app/src/App.jsx
+++ React/my-app/src/App.jsx
@@ -1,15 +1,14 @@
 import Footer from "./section/Footer"
 import Header from "./section/Header"
 import Hero from "./section/Hero"
-import './global.css'
 
 export const App=()=>{
 
   return (
-     <div className="app">
+     <>
       <Header/>
       <Hero/>
       <Footer/>
-    </div>
+    </>
 )
 }
