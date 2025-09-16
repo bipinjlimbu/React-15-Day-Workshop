118b818c7746adf2cf32667101fce6fba640f0ff
bipinjlimbu <bipinlimbu712@gmail.com>
2025-09-16 10:41:46 +0545

    Counter with Event Completed in React

---
+++ React/my-app/src/pages/Counter.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react';
+import myCout from "../style/Counter.module.css";
+
+const Counter = () => {
+  const [count, setCount] = useState(0);
+
+  const increment = () => {
+    setCount(count + 1);
+  };
+
+  const decrement = () => {
+    setCount(count - 1);
+  };
+
+  return (
+    <div className={myCout.counter}>
+      <h2>Counter</h2>
+      <p>Count: {count}</p>
+      <button onClick={increment}>Increment</button>
+      <button onClick={decrement}>Decrement</button>
+    </div>
+  );
+};
+export default Counter;
