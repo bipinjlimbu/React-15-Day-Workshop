3acf7759b5eadc36ab464a829fbc0926ae137d24
bipinjlimbu <bipinlimbu712@gmail.com>
2025-09-23 20:04:43 +0545

    Getting Data CSS Completed in React

--- React/my-app/src/components/GetDoor.jsx
+++ React/my-app/src/components/GetDoor.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import tdl from "../style/TodoList.module.css"
 
 const TodoList=()=>{
 const [data, setData] = useState([]);
@@ -6,7 +7,6 @@ const [data, setData] = useState([]);
         const response = await fetch(`https://jsonplaceholder.typicode.com/todos/`);
         const parsedResponse = await response.json();
         setData(parsedResponse);
-        console.log(parsedResponse);
     }
 
     useEffect(() => {
@@ -14,9 +14,9 @@ const [data, setData] = useState([]);
     }, []);
 
     return (
-          <div>
-            <h1>Todo List</h1>
-            <div>
+          <div className={tdl.container}>
+            <h1 className={tdl.heading}>Todo List</h1>
+            <div className={tdl.todoList}>
                 {
                   data.map((item,index)=><TodoCard key={index} data={item}/>)
                 }
@@ -27,9 +27,7 @@ const [data, setData] = useState([]);
 
 const TodoCard = ({ data }) => {
     return (
-        <div style={{
-            backgroundColor:"gray"
-        }}>
+        <div className={tdl.todoCard}>
             <h2>ID:{data.id}</h2>
             <h2>Title:{data.title}</h2>
             <h2>Completed:{data.completed ? "Yes" : "No"}</h2>
